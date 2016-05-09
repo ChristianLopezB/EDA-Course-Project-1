128ed8119e3a13ea1d65b6d3b1249c55d2f85f31
ChristianLopezB <gtcelb@gmail.com>
2016-05-09 15:49:10 -0400

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,19 +1,23 @@
-setwd("C:\\Users\\Christian\\Desktop\\exdata-data-household_power_consumption")
-
+#READ FILE FROM WD
 data<-read.table("household_power_consumption.txt", sep= ";", header = TRUE)
+
+#SUBSET DAYS
 data21<-subset(data, data$Date=="1/2/2007" )
 daat22<-subset(data,  data$Date=="2/2/2007")
 data2<-rbind(data21, daat22)
 rm(data)
+
+#CHANGE DATE FORMAT
 Date_Time<-strptime (paste(data2[,1], data2[,2]), format= "%d/%m/%Y %H:%M:%S")
 data2<-cbind(data2,Date_Time)                
 rm(Date_Time)                
 data3<-data2[, 3:10]
 rm(data2)
 
+#OPEN PNG FILE#
 png(file= "plot1.png" ,width = 480, height = 480 )
 
-#PLOT1
+#GENERATE PLOT1
 hist(as.numeric(as.character(data3$ Global_active_power )), col="red", 
      xlab= "Global Active Power (kilowatts)", breaks = 12, main = "Global Active Power")
 
